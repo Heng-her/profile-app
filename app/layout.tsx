d3990f554af60047c0032abe86a2379e93051d5b
Her bunheng <hengnun68@gmail.com>
2026-08-13 20:38:13 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SplineBackground } from "@/components/spline";
+// import { SplineBackground } from "@/components/spline";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,12 +18,12 @@ export const metadata: Metadata = {
   description: "Interactive 3D Profile Application",
 };
 
-const splineUrl = {
-  bg: {
-    link: "https://prod.spline.design/OZIFEJyygtHz0WqQ/scene.splinecode",
-    code: "OZIFEJyygtHz0WqQ",
-  },
-};
+// const splineUrl = {
+//   bg: {
+//     link: "https://prod.spline.design/OZIFEJyygtHz0WqQ/scene.splinecode",
+//     code: "OZIFEJyygtHz0WqQ",
+//   },
+// };
 
 export default function RootLayout({
   children,
@@ -37,10 +37,10 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col relative bg-black text-white">
         {/* Global 3D Background */}
-        <SplineBackground
+        {/* <SplineBackground
           url={splineUrl.bg.link}
           cacheKey={splineUrl.bg.code}
-        />
+        /> */}
 
         {/* Main Content Layer (allows mouse events to pass through to 3D Spline canvas) */}
         <div className="relative z-20 flex-1 flex flex-col pointer-events-none">
